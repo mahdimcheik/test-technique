f94e649322a782522726de17684713abe677d634
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2024-07-21 14:44:14 +0200

    navigation links , active with actual url

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { NavbarLinkModel } from '../../models/navbar-link-model';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, take } from 'rxjs';
+import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   activeIndex = new BehaviorSubject<number>(0);
+  router = inject(Router);
+
   links: NavbarLinkModel[] = [
     {
       navText: 'Ecran 1',
@@ -21,5 +24,21 @@ export class NavbarComponent {
   ];
   changeActive(index: number) {
     this.activeIndex.next(index);
+  }
+
+  ngOnInit(): void {
+    this.router.events.pipe(take(1)).subscribe((response) => {
+      if (response instanceof NavigationStart) {
+        console.log(response.url);
+
+        if (response.url === this.links[0].navPath) {
+          this.activeIndex.next(0);
+          console.log('0');
+        }
+        if (response.url === this.links[1].navPath) {
+          this.activeIndex.next(1);
+        }
+      }
+    });
   }
 }
